a48931a34b37b16fac76c959e733a96bf4543a13
dgomatha <dgomatha@cisco.com>
2026-01-12 10:31:05 +0530

    implimented CRUD operations

--- main_project/contacts-backend/controllers/contactController.js
+++ main_project/contacts-backend/controllers/contactController.js
@@ -1,11 +1,15 @@
+
 //description Get all controller functions for contacts
 // @route  GET /api/contacts
 // access Public
 
+
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModels");
 
 const getContacts = asyncHandler(async (req, res) => {
-    res.json({ "message": "Get all contacts" });
+    const contacts = await Contact.find();
+    res.json(contacts);
 })
 
 const createContact = asyncHandler(async (req, res) => {
@@ -16,19 +20,43 @@ const createContact = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("All fields are mandatory")
     }
+    const contact = await Contact.create({
+        name,
+        email,
+        phone
+    });
 
-    res.json({ "message": "Create a new contact" });
+    res.json(contact);
 })
 
 const updateContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
     res.json({ "message": `Update a contact ${req.params.id}` });
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+    res.json(updatedContact);
 })
 const deleteContact = asyncHandler(async (req, res) => {
-    res.json({ "message": `Delete a contact ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    await Contact.findByIdAndDelete(req.params.id);
+
+    res.json(contact);
 })
 
 const getContactById = asyncHandler(async (req, res) => {
-    res.json({ "message": `Get contact by ID ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    res.json(contact);
 })
 
 
